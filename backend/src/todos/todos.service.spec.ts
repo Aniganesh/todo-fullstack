221db9b28b056b258a86d3e441ba4bdcf3c7ff82
aniganesh <aniganesh741@gmail.com>
2024-05-23 13:03:07 +0530

    test todo service

--- backend/src/todos/todos.service.spec.ts
+++ backend/src/todos/todos.service.spec.ts
@@ -2,33 +2,70 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { TodosService } from './todos.service';
 import { Todo } from 'src/models/todos.entity';
 import { getRepositoryToken } from '@nestjs/typeorm';
+import { Users } from 'src/models/users.entity';
+import { mockRepositoryFactory } from 'src/testing-helpers';
 import { Repository } from 'typeorm';
 
 describe('TodosService', () => {
   let service: TodosService;
+  let repository: Repository<Todo>;
+  const userId = 'user-id';
+
+  const user: Users = {
+    email: 'user@email.com',
+    id: userId,
+    createDateTime: new Date(),
+    lastChangedDateTime: new Date(),
+    name: 'username',
+    password: 'password',
+  };
 
   beforeEach(async () => {
-    const mockRepository: Partial<Repository<Todo>> = {
-      save: jest.fn(),
-      find: jest.fn(),
-      findOne: jest.fn(),
-      delete: jest.fn(),
-      create: jest.fn(),
-    };
     const module: TestingModule = await Test.createTestingModule({
       providers: [
         TodosService,
         {
           provide: getRepositoryToken(Todo),
-          useValue: mockRepository,
+          useFactory: mockRepositoryFactory,
         },
       ],
     }).compile();
 
     service = module.get<TodosService>(TodosService);
+    repository = module.get<Repository<Todo>>(getRepositoryToken(Todo));
   });
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  it('should return todos', async () => {
+    // arrange
+    const todos: Todo[] = [
+      {
+        id: Date.now().toString(),
+        createDateTime: new Date(),
+        lastChangedDateTime: new Date(),
+        description: 'description',
+        status: 'todo',
+        title: 'title',
+        user,
+      },
+    ];
+
+    jest.spyOn(repository, 'createQueryBuilder').mockImplementation(
+      () =>
+        ({
+          andWhere: jest.fn().mockReturnThis(),
+          addSelect: jest.fn().mockReturnThis(),
+          getMany: jest.fn().mockReturnValue(todos),
+        }) as any,
+    );
+
+    //act
+    const result = await service.getAllForUser(user);
+
+    // assert
+    expect(result).toBe(todos);
   });
 });
