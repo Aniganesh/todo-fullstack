5dfbaa0251b268b4a96d6da01d47dd2824f1dc6b
aniganesh <aniganesh741@gmail.com>
2024-06-12 22:25:49 +0530

    refactor

--- frontend/src/api/Auth/types.ts
+++ frontend/src/api/Auth/types.ts
@@ -1,31 +1,8 @@
-import { BaseEntity } from "@/types";
+import { Image, BaseEntity } from "@/types";
 
 export interface LoginData {
   email: string;
   password: string;
-}
-
-interface Image {
-  asset_id: string;
-  public_id: string;
-  version: number;
-  version_id: string;
-  signature: string;
-  width: string;
-  height: string;
-  format: string;
-  resource_type: "image";
-  created_at: string;
-  tags: [];
-  bytes: number;
-  type: "upload";
-  etag: string;
-  placeholder: false;
-  url: string;
-  secure_url: string;
-  folder: "";
-  original_filename: string;
-  api_key: string;
 }
 
 export interface User extends BaseEntity {
--- frontend/src/types.ts
+++ frontend/src/types.ts
@@ -16,3 +16,27 @@ export type DeepPartial<T> = T extends object
       [P in keyof T]?: DeepPartial<T[P]>;
     }
   : T;
+
+
+export interface Image {
+  asset_id: string;
+  public_id: string;
+  version: number;
+  version_id: string;
+  signature: string;
+  width: string;
+  height: string;
+  format: string;
+  resource_type: "image";
+  created_at: string;
+  tags: [];
+  bytes: number;
+  type: "upload";
+  etag: string;
+  placeholder: false;
+  url: string;
+  secure_url: string;
+  folder: "";
+  original_filename: string;
+  api_key: string;
+}
